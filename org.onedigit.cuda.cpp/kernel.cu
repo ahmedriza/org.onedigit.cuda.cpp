b0dce0e50133483d89bd48884e9a8540bacf5d5c
unknown <RIZAA@C0155.daiwaeurope.local>
2012-12-17 17:14:20 +0000

    commit

---
+++ org.onedigit.cuda.cpp/kernel.cu
@@ -0,0 +1,88 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <iostream>
+#include <stdio.h>
+
+#define N 10
+
+__global__ void add(int a, int b, int *c)
+{
+	*c = a + b;
+}
+
+__global__ void vecAddDevice(int* a, int* b, int* c)
+{
+	int tid = blockIdx.x; 
+	if (tid < N) {
+		c[tid] = a[tid] + b[tid];
+	}
+}
+
+void vectAdd()
+{
+	int a[N], b[N], c[N];
+	int *dev_a, *dev_b, *dev_c;
+	cudaMalloc( (void**)&dev_a, N * sizeof(int) );
+	cudaMalloc( (void**)&dev_b, N * sizeof(int) );
+	cudaMalloc( (void**)&dev_c, N * sizeof(int) );
+	for (int i = 0; i < N; i++) {
+		a[i] = -i;
+		b[i] = i * i;
+	}
+	cudaMemcpy(dev_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_b, b, N * sizeof(int), cudaMemcpyHostToDevice);
+
+	vecAddDevice<<<N, 1>>>(dev_a, dev_b, dev_c);
+	
+	cudaMemcpy(c, dev_c, N * sizeof(int), cudaMemcpyDeviceToHost);
+	for (int i = 0; i < N; i++) {
+		std::cout << a[i] << "+" << b[i] << " = " << c[i] << std::endl;
+	}
+}
+
+void test()
+{
+	int count;
+	int error =	cudaGetDeviceCount(&count);
+    if (error != cudaSuccess) {
+        printf("cudaGetDeviceCount returned error code %d, line(%d)\n", error, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+    std::cout << "Number of devices = " << count << std::endl;
+    cudaDeviceProp prop;
+    for (int i = 0; i < count; i++) {
+		error = cudaGetDeviceProperties(&prop, i);
+		if (error == cudaSuccess) {
+			std::cout << "Name: " << prop.name << std::endl;
+			std::cout << "Total Global Mem: " << prop.totalGlobalMem << std::endl;
+		}
+	}
+
+	int c;
+	int* dev_c;
+	error = cudaMalloc( (void**)&dev_c, sizeof(int) );
+    if (error != cudaSuccess) {
+        printf("cudaMalloc returned error code %d, line(%d)\n", error, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+	
+	add<<<1, 1>>>(2, 7, dev_c);
+	
+	error =	cudaMemcpy(&c, dev_c, sizeof(int), cudaMemcpyDeviceToHost);
+    if (error != cudaSuccess) {
+        printf("cudaMemcpy returned error code %d, line(%d)\n", error, __LINE__);
+        exit(EXIT_FAILURE);
+    }
+
+	std::cout << "2 + 7 = " << c << std::endl;
+	cudaFree(dev_c);
+}
+
+int main()
+{
+	vectAdd();
+	return 0;
+}
+
